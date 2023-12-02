263ad1b24faa56deb5e46a2d50b21789f424969a
matyiandras <a2001matyas@gmail.com>
2023-12-02 19:21:19 +0100

    Update CORS origin in app.use()

--- functions/index.js
+++ functions/index.js
@@ -21,13 +21,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({ origin: ['http://localhost:3000', 'https://tshirt-generator-poc.web.app'] }));
 
 app.post('/generate-image', async (req, res) => {
   try {
