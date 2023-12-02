d17cb1fb5be34968fac331a09b1c3c0c77c115eb
matyiandras <a2001matyas@gmail.com>
2023-12-02 19:04:16 +0100

    Add CORS headers and create endpoint for generating t-shirt designs

--- functions/index.js
+++ functions/index.js
@@ -23,6 +23,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/generate-image', async (req, res) => {
   try {
     const response = await openai.images.generate({
@@ -37,6 +43,26 @@ app.post('/generate-image', async (req, res) => {
     image_url = response.data[0].b64_json;
 
     res.send({ url: image_url});
+  } catch (error) {
+    console.error('Error calling OpenAI API:', error);
+    res.status(500).send('Error generating image');
+  }
+});
+
+app.post('/generate-tshirt-designs', async (req, res) => {
+  try {
+    const response = await openai.images.generate({
+      prompt: req.body.prompt,
+      size: "512x512",
+      n: 4,
+      response_format: "b64_json",
+    });
+
+    console.log(req.body.prompt);
+
+    design_urls = response.data.map((design) => design.b64_json);
+
+    res.send({ urls: design_urls});
   } catch (error) {
     console.error('Error calling OpenAI API:', error);
     res.status(500).send('Error generating image');
